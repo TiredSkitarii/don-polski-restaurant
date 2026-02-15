cf60420bcdf5ac8543913fbb69f21d794ad6b480
TiredSkitarii <jakob.mclaughlin@googlemail.com>
2026-02-15 15:55:57 +0000

    created booking form in Website

---
+++ website/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from restaurant_booking.models import Booking_Info
+from datetime import datetime, timedelta, time
+
+# generates times for the booking form. originally written by Bear81
+
+
+def generate_time_choices(start=time(12, 0), end=time(22, 0), interval=30):
+    current = datetime.combine(datetime.today(), start)
+    end_dt = datetime.combine(datetime.today(), end)
+    choices = []
+    while current <= end_dt:
+        t = current.time()
+        choices.append((t.strftime("%H:%M"), t.strftime("%H:%M")))
+        current += timedelta(minutes=interval)
+    return choices
+
+
+class BookingForm(forms.ModelForm):
+    booking_date = forms.DateField(
+        widget=forms.DateInput(attr={'type': 'date', 'class': 'form-control'})
+    )
+    booking_time = forms.ChoiceField(
+        choices = generate_time_choices(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+
+class Meta:
+    model = Booking_Info
+    fields = ['number_of_guests']
