8aa849f08b078eed42038139a202b6d6182a861b
TiredSkitarii <jakob.mclaughlin@googlemail.com>
2026-02-19 23:28:06 +0000

    added missing allowed host

--- don_polski/settings.py
+++ don_polski/settings.py
@@ -32,6 +32,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
