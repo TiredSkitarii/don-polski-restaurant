c94ca9471cf4f6f8126adeac98c1af3d384b35ba
TiredSkitarii <jakob.mclaughlin@googlemail.com>
2026-06-10 13:24:16 +0100

    Updated Secret Key

--- don_polski/settings.py
+++ don_polski/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kc5_i**i=cc(l5ikrxv-g1e-@18jjv8@&*ozm*kc%d^wgs9no@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
